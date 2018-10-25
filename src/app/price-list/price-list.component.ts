4ba8f6d2f4a07274b1d0cc8706fd1e7a57c6d522
Tharindu Kumarasiri <tharindukumarasiri@users.noreply.github.com>
2018-10-25 09:27:38 +0530

    Add some changes to price list

--- src/app/price-list/price-list.component.ts
+++ src/app/price-list/price-list.component.ts
@@ -1,5 +1,6 @@
 import { PriceListService } from './../price-list.service';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-price-list',
@@ -8,6 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PriceListComponent implements OnInit {
   //priceList$;
+  show;
+  show1 = false;
+  show2 = false;
 
   //constructor(private PriceListService: PriceListService) {
   //  this.priceList$ = this.PriceListService.getAll();
@@ -16,12 +20,23 @@ export class PriceListComponent implements OnInit {
   priceListMethods = [
     { id: 1, name: 'Teak - Super Luxury Class - Both Non RCT and RCT (Felled volume basis)'},
     { id: 2, name: 'Super Luxury Class(Nadun)'},
-    { id: 2, name: 'Super Luxury Class - Ebony & Calamader'},
-    { id: 2, name: 'Luxury Class - Satin'},
-    { id: 2, name: 'Luxury Class - Halmilla, Rose wood'},
-    { id: 2, name: 'Luxury Class - Milla, Jak'}
+    { id: 3, name: 'Super Luxury Class - Ebony & Calamader'},
+    { id: 4, name: 'Luxury Class - Satin'},
+    { id: 5, name: 'Luxury Class - Halmilla, Rose wood'},
+    { id: 6, name: 'Luxury Class - Milla, Jak'}
   ];
 
+  toggleShow(){
+    if(this.show==1){
+      this.show1 = true;     
+    }else if(this.show==2){
+      this.show2 = true; 
+    } 
+  }
+
+  selected(event: any){
+    this.show = event.target.value;
+  }
   
 
   ngOnInit() {
